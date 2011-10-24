35839fe72922af11da51f112ee6198b36144d49c
Hans Reichenbach <HansReich25@gmail.com>
2011-10-24 14:32:22 -0400

    first commit

---
+++ Project LAWN/src/edu/gatech/cs4261/LAWN/ProjectLAWNActivity.java
@@ -0,0 +1,13 @@
+package edu.gatech.cs4261.LAWN;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ProjectLAWNActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
