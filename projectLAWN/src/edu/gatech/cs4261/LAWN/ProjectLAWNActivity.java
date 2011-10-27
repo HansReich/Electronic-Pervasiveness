b337883d97fe5a28a99b9cc8a8ceee794578f7fa
Hans Reichenbach <HansReich25@gmail.com>
2011-10-27 19:07:51 -0400

    added the button listeners to the main method

--- projectLAWN/src/edu/gatech/cs4261/LAWN/ProjectLAWNActivity.java
+++ projectLAWN/src/edu/gatech/cs4261/LAWN/ProjectLAWNActivity.java
@@ -2,12 +2,48 @@ package edu.gatech.cs4261.LAWN;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class ProjectLAWNActivity extends Activity {
+	/** constants needed*/
+	private static final String TAG = "Project LAWN Main";
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        /* set up the button listeners*/
+		Button btnScan = (Button)findViewById(R.id.btnScan);
+		btnScan.setOnClickListener(btnScanListener);
+		Button btnHistory = (Button)findViewById(R.id.btnHistory);
+		btnHistory.setOnClickListener(btnHistoryListener);
+		Button btnPref = (Button)findViewById(R.id.btnPref);
+		btnPref.setOnClickListener(btnPrefListener);
     }
+    
+    /** what to do when Scan is clicked */
+    private OnClickListener btnScanListener = new OnClickListener() {
+		public void onClick(View v) {
+			Log.d(TAG, "Scan clicked");
+		}
+    };
+    
+    /** what to do when History is clicked */
+    private OnClickListener btnHistoryListener = new OnClickListener() {
+		public void onClick(View v) {
+			Log.d(TAG, "History clicked");
+		}
+    };
+    
+    /** what to do when Preferences is clicked */
+    private OnClickListener btnPrefListener = new OnClickListener() {
+		public void onClick(View v) {
+			Log.d(TAG, "Preferences clicked");
+		}
+    };
 }
