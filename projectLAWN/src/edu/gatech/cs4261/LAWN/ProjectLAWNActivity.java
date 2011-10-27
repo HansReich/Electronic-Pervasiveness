c1f680763e5c78c179f99eba73e3a90e14826d7b
Hans Reichenbach <HansReich25@gmail.com>
2011-10-27 18:17:07 -0400

    force close on main

--- projectLAWN/src/edu/gatech/cs4261/LAWN/ProjectLAWNActivity.java
+++ projectLAWN/src/edu/gatech/cs4261/LAWN/ProjectLAWNActivity.java
@@ -8,6 +8,6 @@ public class ProjectLAWNActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.prefs);
+        setContentView(R.layout.main);
     }
 }
